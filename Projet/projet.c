2630c207cc3b4486afc63423032e4950e3a264ba
Abdelhakim Baalla <abdelhakimbaalla40@gmail.com>
2024-09-10 18:33:27 +0100

    Create a Struct for storing information of students

--- Projet/projet.c
+++ Projet/projet.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
+
+#define MAX_ETUDIANTS 1000
+#define MAX_LENGTH 50
+
+
+typedef struct{
+    int id;
+    char nom[MAX_LENGTH];
+    char prenom[MAX_LENGTH];
+    char date_de_naissances[MAX_LENGTH];
+    char departement[MAX_LENGTH];
+    float note_generale;
+}Etudiant;
 
 int main(){
     return 0;
