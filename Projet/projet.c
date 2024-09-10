b8f4adc7781af13eb965d6106f246d8d86dcb1ac
Abdelhakim Baalla <abdelhakimbaalla40@gmail.com>
2024-09-10 18:58:57 +0100

    Create a functoin of a principale menu

--- Projet/projet.c
+++ Projet/projet.c
@@ -16,5 +16,58 @@ typedef struct{
 }Etudiant;
 
 int main(){
+    menu_principale();
     return 0;
+}
+
+
+void menu_principale(){
+    int reponse;
+    do{
+    printf("\nApplication de Gestion des étudiants dans une université\n");
+    printf("1.Ajouter un etudiant\n");
+    printf("2.Modifier un etudiant\n");
+    printf("3.Supprimer un etudiant\n");
+    printf("4. Afficher les details d'un etudiant\n");
+    printf("5. Calculer la moyenne generale\n");
+    printf("6. Statistiques\n");
+    printf("7. Rechercher un etudiant\n");
+    printf("8. Trier les etudiants\n");
+    printf("9. Quitter\n");
+    printf("Votre Choix: ");
+    scanf("%d", &reponse);
+
+      switch(reponse){
+        case 1:
+            ajouter_un_etudiant();
+            break;
+        case 2:
+            modifier_les_informations();
+            break;
+        case 3:
+            supprimer_un_etudiant();
+            break;
+        case 4:
+            afficher_les_details();
+            break;
+        case 5:
+            calculer_le_moyenne();
+            break;
+        case 6:
+            statistique();
+            break;
+        case 7:
+            chercher_un_etudiant();
+            break;
+        case 8:
+            trier_les_etudiants();
+            break;
+        case 9:
+            printf("Au revoir!\n");
+            break;
+        default:
+            printf("Choix invalid saisez une autre reponse existe dans le menu\n");
+            break;
+      }
+    }while(reponse != 9);
 }
